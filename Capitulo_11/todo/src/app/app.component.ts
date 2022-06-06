1df0d5a8cff3234c2d3f5e4c6402fc541626dd7a
EdiJunior88 <er.moura88@yahoo.com.br>
2022-06-06 17:52:35 -0300

    Removendo um item

---
+++ Capitulo_11/todo/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {}
+
+  markAsUndone() {}
+}

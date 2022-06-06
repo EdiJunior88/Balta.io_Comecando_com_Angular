d39b01f16624eaba90a20ee7a2115adee8efaf15
EdiJunior88 <er.moura88@yahoo.com.br>
2022-06-06 11:35:50 -0300

    Exibindo Dados

---
+++ Capitulo_08/todo/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public todos: any[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push('Passear com o cachorro');
+    this.todos.push('ir ao supermercado');
+    this.todos.push('cortar o cabelo');
+  }
+}

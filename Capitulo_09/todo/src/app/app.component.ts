4f4619d1bc91788c352391cb14ecffe4ec29ffc1
EdiJunior88 <er.moura88@yahoo.com.br>
2022-06-06 13:55:16 -0300

    Objetos Complexos

---
+++ Capitulo_09/todo/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  public title: string = 'Minhas Tarefas';
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+  }
+}
